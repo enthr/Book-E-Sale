605525c1f128eeb677110c3f1e70bbc29ab63c7b
Jaimin Prajapati <enthralall@gmail.com>
2022-06-13 17:55:18 +0530

    Removed Unecessary Packages

--- BookStore/BookStore.API/Startup.cs
+++ BookStore/BookStore.API/Startup.cs
@@ -25,27 +25,38 @@ namespace BookStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            services.AddCors(opt =>
+            {
+                opt.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore", Version = "v3" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore"));
+            }
 
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore v1"));
-
-            //// enable cross origin to allow access to all requests for development environment.
-            app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // enable cross origin to allow access to all requests for development environment.
+            app.UseCors();
 
             app.UseAuthorization();
 
